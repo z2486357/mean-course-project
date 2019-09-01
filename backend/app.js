a18ffbe21254ba834cfe26d675c6448b20dc6dad
z2486357 <z2486357@gmail.com>
2019-08-31 21:54:19 -0400

    update backend

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('111111111111111');
-});
-
-app.use((req, res, next) => {
-  res.send('expresssssssssss');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    { id: "asdad", title: "1 server post", content: "from server" },
+    { id: "qwdqd", title: "2 server post", content: "from server -2" },
+    { id: "ewqeqwe", title: "3 server post", content: "from server -3" },
+    { id: "ghsrht", title: "4 server post", content: "from server -4" },
+    { id: "afcafa", title: "5 server post", content: "from server -5" }
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully!",
+    posts: posts
+  });
 });
 
 module.exports = app;
