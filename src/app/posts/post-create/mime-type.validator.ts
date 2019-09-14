16257339cfa8a8f37a4d22dddcf3d07cd5f84c3c
z2486357 <z2486357@gmail.com>
2019-09-14 18:27:28 -0400

    Update mime-type.validator.ts

--- src/app/posts/post-create/mime-type.validator.ts
+++ src/app/posts/post-create/mime-type.validator.ts
@@ -1,9 +1,9 @@
 import { AbstractControl } from "@angular/forms";
 import { Observable, Observer, of } from "rxjs";
 
-export const mimeType = (
-  control: AbstractControl
-): Promise<{ [key: string]: any }> | Observable<{ [key: string]: any }> => {
+
+// Validator that the chosen file need to be img file
+export const mimeType = (control: AbstractControl): Promise<{ [key: string]: any }> | Observable<{ [key: string]: any }> => {
   if (typeof (control.value) === 'string') {
     return of(null);
   }
