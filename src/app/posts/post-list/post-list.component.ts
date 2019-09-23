09bebf416cad3d1a1ca792250b2310b3c5ad8b8e
z2486357 <z2486357@gmail.com>
2019-09-22 20:26:48 -0400

    Update post-list.component.ts

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -11,6 +11,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class PostListComponent implements OnInit {
 
   isLoading = false;
+  
   get posts() { return this.postService.getPosts(); }
   constructor(private postService: PostsService,
     private router: Router,
