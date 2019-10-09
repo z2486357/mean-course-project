e474f213ce2ccfa6eea4c32ff02a5decf3b4fce7
z2486357 <z2486357@gmail.com>
2019-10-09 16:30:31 -0400

    add sign up

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  isLoading = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  signup(form: NgForm){
+    console.log(form.value);
+  }
 }
