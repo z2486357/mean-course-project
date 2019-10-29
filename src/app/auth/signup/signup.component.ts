ddb89ab4285c95e0cfd03e38d7c9485ac3263acb
z2486357 <z2486357@gmail.com>
2019-10-28 19:17:50 -0700

    finish signup function

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -9,12 +10,16 @@ import { NgForm } from '@angular/forms';
 export class SignupComponent implements OnInit {
   isLoading = false;
 
-  constructor() { }
+  constructor(private authService:AuthService) { }
 
   ngOnInit() {
   }
 
   signup(form: NgForm){
-    console.log(form.value);
+    if(form.invalid){
+      return;
+    }
+    this.authService.createUser(form.value.email,form.value.password);
+    //console.log(form.value);
   }
 }
