9e702881e5b36dcaaf422bd6c07aaa9b2f5d4423
z2486357 <z2486357@gmail.com>
2019-11-01 17:27:00 -0700

    auth backend

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const Post = require('../models/post');
+const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
 
@@ -26,7 +27,7 @@ const storage = multer.diskStorage({
   }
 });
 
-router.post("", multer({ storage: storage }).single("image"), (req, res, next) => {
+router.post("",checkAuth, multer({ storage: storage }).single("image"), (req, res, next) => {
   const url = req.protocol + "://" + req.get("host");
   const post = new Post({
     title: req.body.title,
@@ -72,7 +73,7 @@ router.get("", (req, res, next) => {
 
 });
 
-router.put("/:id", multer({ storage: storage }).single("image"), (req, res, next) => {
+router.put("/:id",checkAuth, multer({ storage: storage }).single("image"), (req, res, next) => {
   let imagePath=req.body.imagePath;
   //console.log(imagePath)
   if (req.file) {
@@ -101,7 +102,7 @@ router.get("/:id", (req, res, next) => {
   })
 })
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id",checkAuth, (req, res, next) => {
   //console.log(req.params.id);
   Post.deleteOne({ _id: req.params.id }).then((result) => {
     //console.log(result);
