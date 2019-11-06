8161832afa8b85b7c56057ced80085a6b815004b
z2486357 <z2486357@gmail.com>
2019-11-05 17:21:20 -0800

    add auth guard

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  get isAuth(){return this.authService.getAuthStatus();}
+
+  constructor(private authService:AuthService,
+    private router:Router){}
+
+  canActivate(route: import("@angular/router").ActivatedRouteSnapshot,
+    state: import("@angular/router").RouterStateSnapshot): boolean |
+    import("@angular/router").UrlTree | import("rxjs").Observable<boolean |
+    import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
+      if(!this.isAuth){
+        this.router.navigate(['/login']);
+      }
+      return this.isAuth;
+
+  }
+
+}
