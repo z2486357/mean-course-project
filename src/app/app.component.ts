10f53f3625428a291cde0473cd33bea8bbd8cf3d
z2486357 <z2486357@gmail.com>
2019-11-24 19:36:35 -0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { AuthService } from './auth/auth.service';
 export class AppComponent implements OnInit {
 
   constructor(private authService: AuthService) { }
+
   ngOnInit() {
     this.authService.autoAuthUser();
   }
