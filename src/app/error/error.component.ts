a87c60b0830baeeae5bc17291c8365e5aa287fbd
z2486357 <z2486357@gmail.com>
2019-11-26 14:25:19 -0800

    update error message

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
-  templateUrl:'./error.component.html'
+  templateUrl: './error.component.html'
 })
 
 export class ErrorComponent {
-  message='An Unknown error occurred!'
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { message: string }) { }
 }
