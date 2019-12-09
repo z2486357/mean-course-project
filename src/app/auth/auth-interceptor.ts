6f135db62fda7543a1dab516377e31f321da6a7e
z2486357 <z2486357@gmail.com>
2019-12-08 17:11:25 -0800

    Update auth-interceptor.ts

--- src/app/auth/auth-interceptor.ts
+++ src/app/auth/auth-interceptor.ts
@@ -3,14 +3,14 @@ import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthInterceptor implements HttpInterceptor{
+export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authservice:AuthService){}
+  constructor(private authservice: AuthService) { }
 
-  intercept(req:HttpRequest<any>,next:HttpHandler){
-    const authToken=this.authservice.getToken();
-    const authRequest=req.clone({
-      headers:req.headers.set('authorization',"Bearer "+authToken)
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const authToken = this.authservice.getToken();
+    const authRequest = req.clone({
+      headers: req.headers.set('authorization', "Bearer " + authToken)
     });
     return next.handle(authRequest);
   }
