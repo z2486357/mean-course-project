913df1efa88f6f6435e2e6e42f6dd1bdf0565b95
z2486357 <z2486357@gmail.com>
2019-12-12 17:04:17 -0800

    prepare to deploy

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 // npm install --save bcrypt
 // for changing password to unreadable string
 const jwt = require('jsonwebtoken');
