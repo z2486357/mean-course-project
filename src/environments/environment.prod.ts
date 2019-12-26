c2fea58919e4b5bea110ee866b075c95234ebfa0
z2486357 <z2486357@gmail.com>
2019-12-25 23:03:12 -0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:"http://postsboard-env-new.3ximc2apxn.us-east-2.elasticbeanstalk.com/api"
+  apiUrl: "http://postsboard-env-new.3ximc2apxn.us-east-2.elasticbeanstalk.com/api"
 };
