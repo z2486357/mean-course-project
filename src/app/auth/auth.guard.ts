b1b53bccfe97e5bfc2ad60a368cc5b51793de8e6
z2486357 <z2486357@gmail.com>
2019-12-27 23:10:34 -0800

    Update auth.guard.ts

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -4,19 +4,19 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  get isAuth(){return this.authService.getAuthStatus();}
+  get isAuth() { return this.authService.getAuthStatus(); }
 
-  constructor(private authService:AuthService,
-    private router:Router){}
+  constructor(private authService: AuthService,
+    private router: Router) { }
 
   canActivate(route: import("@angular/router").ActivatedRouteSnapshot,
     state: import("@angular/router").RouterStateSnapshot): boolean |
     import("@angular/router").UrlTree | import("rxjs").Observable<boolean |
     import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
-      if(!this.isAuth){
-        this.router.navigate(['/auth/login']);
-      }
-      return this.isAuth;
+    if (!this.isAuth) {
+      this.router.navigate(['/auth/login']);
+    }
+    return this.isAuth;
 
   }
 
